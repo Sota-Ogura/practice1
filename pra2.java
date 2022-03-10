3814daf2d698257976eee6a318817c35200668c9
Sota Ogura <s.ogura.sys21@morijyobi.ac.jp>
2022-03-10 10:24:39 +0900

    pra2.javaの更新
    Hello Worldを100回出力するプログラム

--- pra2.java
+++ pra2.java
@@ -1,6 +1,11 @@
 public class pra2 {
 	public static void main ( String[] arg) {
-		System.out.println("Hello World");
+		int i;
+		i = 0;
+		while( i > 100 ) {
+			System.out.println("Hello World");
+			i = i + 1;
+		}
 	}
 }
 
